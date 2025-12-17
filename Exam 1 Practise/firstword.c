2855c58ebccbeab5736bb4a0fd8ca4494c685b87
Parid <ridigjerasi@gmail.com>
2025-12-17 23:46:43 +0100

    firstword.c

--- Exam 1 Practise/firstword.c
+++ Exam 1 Practise/firstword.c
@@ -3,7 +3,6 @@ include <unistd.h>
 int main (int argc, char **argv){
     int i = 0;
 
-
     if(argc==2){
 
         while(argv[1][i] == 32 || argv[1][i] == 9)
@@ -13,7 +12,7 @@ int main (int argc, char **argv){
         write(1, &argv[1][i], 1);
         i++;
         }
-            }
+        }
         write(1, "\n", 1);
         return(0);
 }
