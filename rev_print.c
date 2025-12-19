c407fa37eb3bc7c0f0060090b58987b95561da73
Parid <ridigjerasi@gmail.com>
2025-12-19 19:33:03 +0100

    New changes

--- rev_print.c
+++ rev_print.c
@@ -8,15 +8,6 @@
 
 // If the number of parameters is not 1, the program displays a newline.
 
-// Examples:
-
-// $> ./rev_print "zaz" | cat -e
-// zaz$
-// $> ./rev_print "dub0 a POIL" | cat -e
-// LIOP a 0bud$
-// $> ./rev_print | cat -e
-// $
-
 #include <unistd.h>
 
 int main(int argc, char **argv)
