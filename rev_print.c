3c8873f51f17f1e27417f0da59cbc5dded4a1018
Parid <ridigjerasi@gmail.com>
2025-12-19 22:46:53 +0100

    reverse print rev

--- rev_print.c
+++ rev_print.c
@@ -8,22 +8,18 @@
 
 // If the number of parameters is not 1, the program displays a newline.
 
+
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
-    int i;
-    i = 0;
+int main( int argc, char argv){
+    int i = 0;
 
     if(argc == 2){
 
-        while(argv[1][i]) i++;
-
-        while(i > 0){
-            i--;
-            write(1, &argv[1][i], 1);
-        }
+        while(argv[1][i])
+        i++;
+        while(i--)
+        write(1, &argv[1][i], 1);
     }
     write(1, "\n", 1);
-    return(0);
 }
