5a5c54a6e5e14351579bc7598316771fe8ce9669
Parid <ridigjerasi@gmail.com>
2026-03-08 03:00:57 +0100

    update

---
+++ Exam_Practise - Python/Level1/bool.py
@@ -0,0 +1,7 @@
+name = 'ana' # no need to put it in the exam just for testing 
+def mainstring(name: str) -> bool:
+        return name == name[::-1]
+
+print(mainstring(name)) # returns true po bone Berti return False 
+
+
